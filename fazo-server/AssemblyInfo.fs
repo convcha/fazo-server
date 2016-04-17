9ffcaf45bc9c3b211858a62634e16547562360e6
hikitest <hikitanaoki.dev@gmail.com>
2016-04-17 23:56:16 +0900

    Initial commit

---
+++ fazo-server/AssemblyInfo.fs
@@ -0,0 +1,24 @@
+﻿namespace fazo_server.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("fazo-server")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("hikitest")>]
+[<assembly: AssemblyProduct("fazo-server")>]
+[<assembly: AssemblyCopyright("Copyright © hikitest 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: Guid("971bc76d-cdc0-4d71-9c99-8ea570dc0b13")>]
+
+[<assembly: AssemblyVersion("0.0.0.1")>]
+[<assembly: AssemblyFileVersion("0.0.0.1")>]
+
+do
+    ()
---
+++ fazo-server/Startup.fs
@@ -0,0 +1,45 @@
+﻿module Startup
+
+open Owin
+open Microsoft.Owin
+open System
+open System.IO
+open System.Drawing
+open System.Drawing.Imaging;
+open System.Reflection
+open System.Threading.Tasks
+
+let transform (input: string) =
+    sprintf "%s transformed" input
+
+type public Startup() = 
+
+    let handleOwinContext (context:IOwinContext) =
+
+        use writer = new StreamWriter(context.Response.Body)
+
+        match context.Request.Method with
+        | "POST" ->
+            let userName = context.Request.Headers.Get("userName")
+            let fileName = context.Request.Headers.Get("fileName")
+            let currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            let directory = Path.Combine(currentDir, "images", userName);
+            Directory.CreateDirectory(directory) |> ignore
+            let filePath = Path.Combine(directory, fileName);
+            Console.WriteLine(filePath);
+            use img = System.Drawing.Image.FromStream(context.Request.Body);
+            img.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+            let url = Path.Combine("http://localhost:9000/images", userName, fileName);
+            writer.Write(url);
+        | _ ->
+            context.Response.StatusCode <- 400
+            writer.Write("Only POST")
+
+    let owinHandler = fun (context:IOwinContext) (_:Func<Task>) -> 
+        handleOwinContext context; 
+        Task.FromResult(null) :> Task
+
+    member x.Configuration (app:IAppBuilder) = app.UseFileServer(true).Use(owinHandler) |> ignore
+
+let private Main () =
+    printfn "%s" (transform "some input")
